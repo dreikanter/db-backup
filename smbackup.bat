535253c6248cc58c7d5f847eb0f8d21516b61437
Alex Musayev <alex.musayev@gmail.com>
2013-02-17 15:18:56 +0400

    Batches updated.

--- smbackup.bat
+++ smbackup.bat
@@ -1,3 +1,3 @@
 @echo off
 rem smbackup usage example for windows environment
-python %~dp0smbackup.py "%userprofile%\Desktop\Dropbox\Shared Music" "e:\mp3\dropbox-music\%%Y-%%m" %1
+python %~dp0smbackup.py "c:\Users\alex\Desktop\Dropbox\Shared Music" "e:\dropbox-music\%%Y-%%m" %1
